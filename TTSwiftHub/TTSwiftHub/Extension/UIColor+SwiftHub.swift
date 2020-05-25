75b39db7f29ea635ce6e8b3abf3057c0a9bb7024
QDSG <piftao1127@163.com>
2020-05-25 17:18:38 +0800

    主题色管理

---
+++ TTSwiftHub/TTSwiftHub/Extension/UIColor+SwiftHub.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+SwiftHub.swift
+//  TTSwiftHub
+//
+//  Created by QDSG on 2020/5/25.
+//  Copyright © 2020 tTao. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static var text: UIColor {
+        return theme
+    }
+}
