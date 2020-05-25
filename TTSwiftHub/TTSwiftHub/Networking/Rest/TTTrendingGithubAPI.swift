401250535390c208708b469d41d5deb08ff2c4c4
QDSG <piftao1127@163.com>
2020-05-25 10:48:02 +0800

    动态API

---
+++ TTSwiftHub/TTSwiftHub/Networking/Rest/TTTrendingGithubAPI.swift
@@ -0,0 +1,70 @@
+//
+//  TTTrendingGithubAPI.swift
+//  TTSwiftHub
+//
+//  Created by QDSG on 2020/5/19.
+//  Copyright © 2020 tTao. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+enum TTTrendingGithubAPI {
+    case trendingRepositories(language: String, since: String)
+    case trendingDevelopers(language: String, since: String)
+    case languages
+}
+
+extension TTTrendingGithubAPI: TargetType, TTProductAPIType {
+    var baseURL: URL {
+        return Configs.Network.trendingGithubBaseUrl.url!
+    }
+    
+    var path: String {
+        switch self {
+        case .trendingRepositories: return "/repositories"
+        case .trendingDevelopers: return "/developers"
+        case .languages: return "/languages"
+        }
+    }
+    
+    var method: Moya.Method {
+        return .get
+    }
+    
+    var sampleData: Data {
+        var dataUrl: URL?
+        switch self {
+        case .trendingRepositories: dataUrl = R.file.repositoryTrendingsJson()
+        case .trendingDevelopers: dataUrl = R.file.userTrendingsJson()
+        case .languages: dataUrl = R.file.languagesJson()
+        }
+        
+        if let url = dataUrl, let data = try? Data(contentsOf: url) {
+            return data
+        }
+        
+        return Data()
+    }
+    
+    var task: Task {
+        switch self {
+        case .trendingRepositories(let language, let since),
+             .trendingDevelopers(let language, let since):
+            let paras = ["language": language, "since": since]
+            return .requestParameters(parameters: paras, encoding: URLEncoding.default)
+        case .languages:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        return nil
+    }
+    
+    var addXAuth: Bool {
+        return false
+    }
+    
+    
+}
