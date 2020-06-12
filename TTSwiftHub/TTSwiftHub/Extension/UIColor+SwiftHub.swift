10f1997fd3405d8f8f8dd873700520ceb390834c
QDSG <piftao1127@163.com>
2020-06-12 18:01:28 +0800

    extension

--- TTSwiftHub/TTSwiftHub/Extension/UIColor+SwiftHub.swift
+++ TTSwiftHub/TTSwiftHub/Extension/UIColor+SwiftHub.swift
@@ -10,6 +10,18 @@ import UIKit
 
 extension UIColor {
     static var text: UIColor {
-        return theme
+        return themeService.type.associatedObject.primary
+    }
+    
+    static var secondary: UIColor {
+        return themeService.type.associatedObject.primaryDark
+    }
+    
+    var brightnessAdjustedColor: UIColor {
+        var components = self.cgColor.components
+        let alpha = components?.last
+        components?.removeLast()
+        let color = CGFloat(1-(components?.max())! >= 0.5 ? 1.0 : 0.0)
+        return UIColor(red: color, green: color, blue: color, alpha: alpha!)
     }
 }
