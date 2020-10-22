9f13639b4fbf9bc1d24c18e95468956ad5654520
QDSG <piftao1127@163.com>
2020-10-22 18:05:33 +0800

    extension

--- TTSwiftHub/TTSwiftHub/Extension/RxSwift/Observable+Operators.swift
+++ TTSwiftHub/TTSwiftHub/Extension/RxSwift/Observable+Operators.swift
@@ -12,7 +12,7 @@ import RxCocoa
 
 //extension Reactive where Base: UIView {
 //    func tap() -> Observable<Void> {
-//        return tapge
+//        return tapGesture().when(.recognized).mapToVoid()
 //    }
 //}
 
@@ -89,6 +89,12 @@ extension ObservableType where Element == Bool {
     }
 }
 
+extension SharedSequenceConvertibleType {
+    func mapToVoid() -> SharedSequence<SharingStrategy, Void> {
+        return map { _ in }
+    }
+}
+
 extension ObservableType {
     func catchErrorJustComplete() -> Observable<Element> {
         return catchError { _ in
@@ -108,8 +114,96 @@ extension ObservableType {
     }
 }
 
-extension SharedSequenceConvertibleType {
-    func mapToVoid() -> SharedSequence<SharingStrategy, Void> {
-        return map { _ in }
+//https://gist.github.com/brocoo/aaabf12c6c2b13d292f43c971ab91dfa
+extension Reactive where Base: UIScrollView {
+    public var reachedBottom: Observable<Void> {
+        let scrollView = self.base as UIScrollView
+        return self.contentOffset.flatMap { [weak scrollView] (contentOffset) -> Observable<Void> in
+            guard let scrollView = scrollView else { return Observable.empty() }
+            let visibleHeight = scrollView.frame.height - self.base.contentInset.top - scrollView.contentInset.bottom
+            let y = contentOffset.y + scrollView.contentInset.top
+            let threshold = max(0.0, scrollView.contentSize.height - visibleHeight)
+            return (y > threshold) ? Observable.just(()) : Observable.empty()
+        }
     }
+}
+
+// Two way binding operator between control property and variable, that's all it takes {
+
+infix operator <-> : DefaultPrecedence
+
+func nonMarkedText(_ textInput: UITextInput) -> String? {
+    let start = textInput.beginningOfDocument
+    let end = textInput.endOfDocument
+
+    guard let rangeAll = textInput.textRange(from: start, to: end),
+        let text = textInput.text(in: rangeAll) else {
+            return nil
+    }
+
+    guard let markedTextRange = textInput.markedTextRange else {
+        return text
+    }
+
+    guard let startRange = textInput.textRange(from: start, to: markedTextRange.start),
+        let endRange = textInput.textRange(from: markedTextRange.end, to: end) else {
+            return text
+    }
+
+    return (textInput.text(in: startRange) ?? "") + (textInput.text(in: endRange) ?? "")
+}
+
+func <-> <Base>(textInput: TextInput<Base>, variable: BehaviorRelay<String>) -> Disposable {
+    let bindToUIDisposable = variable.asObservable()
+        .bind(to: textInput.text)
+    let bindToVariable = textInput.text
+        .subscribe(onNext: { [weak base = textInput.base] value in
+            guard let base = base else {
+                return
+            }
+
+            let nonMarkedTextValue = nonMarkedText(base)
+
+            /**
+             In some cases `textInput.textRangeFromPosition(start, toPosition: end)` will return nil even though the underlying
+             value is not nil. This appears to be an Apple bug. If it's not, and we are doing something wrong, please let us know.
+             The can be reproed easily if replace bottom code with
+
+             if nonMarkedTextValue != variable.value {
+             variable.value = nonMarkedTextValue ?? ""
+             }
+
+             and you hit "Done" button on keyboard.
+             */
+            if let nonMarkedTextValue = nonMarkedTextValue, nonMarkedTextValue != variable.value {
+                variable.accept(nonMarkedTextValue)
+            }
+            }, onCompleted: {
+                bindToUIDisposable.dispose()
+        })
+
+    return Disposables.create(bindToUIDisposable, bindToVariable)
+}
+
+func <-> <T>(property: ControlProperty<T>, variable: BehaviorRelay<T>) -> Disposable {
+    if T.self == String.self {
+        #if DEBUG
+        fatalError("It is ok to delete this message, but this is here to warn that you are maybe trying to bind to some `rx.text` property directly to variable.\n" +
+            "That will usually work ok, but for some languages that use IME, that simplistic method could cause unexpected issues because it will return intermediate results while text is being inputed.\n" +
+            "REMEDY: Just use `textField <-> variable` instead of `textField.rx.text <-> variable`.\n" +
+            "Find out more here: https://github.com/ReactiveX/RxSwift/issues/649\n"
+        )
+        #endif
+    }
+
+    let bindToUIDisposable = variable.asObservable()
+        .bind(to: property)
+    let bindToVariable = property
+        .subscribe(onNext: { value in
+            variable.accept(value)
+        }, onCompleted: {
+            bindToUIDisposable.dispose()
+        })
+
+    return Disposables.create(bindToUIDisposable, bindToVariable)
 }
