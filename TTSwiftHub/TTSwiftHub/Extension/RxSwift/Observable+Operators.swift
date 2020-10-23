554fb84221b32d70866c35df8b37a73f99cd6c47
QDSG <piftao1127@163.com>
2020-10-23 17:38:13 +0800

    extension

--- TTSwiftHub/TTSwiftHub/Extension/RxSwift/Observable+Operators.swift
+++ TTSwiftHub/TTSwiftHub/Extension/RxSwift/Observable+Operators.swift
@@ -9,12 +9,13 @@
 import Foundation
 import RxSwift
 import RxCocoa
+import RxGesture
 
-//extension Reactive where Base: UIView {
-//    func tap() -> Observable<Void> {
-//        return tapGesture().when(.recognized).mapToVoid()
-//    }
-//}
+extension Reactive where Base: UIView {
+    func tap() -> Observable<Void> {
+        return tapGesture().when(.recognized).mapToVoid()
+    }
+}
 
 protocol OptionalType {
     associatedtype Wrapped
