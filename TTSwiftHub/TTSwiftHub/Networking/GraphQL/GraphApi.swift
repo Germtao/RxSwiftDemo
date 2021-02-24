68a573031df3cdec36eefbbfbcf657fcbf3360f2
tTao <>
2021-02-24 23:35:35 +0800

    todo

---
+++ TTSwiftHub/TTSwiftHub/Networking/GraphQL/GraphApi.swift
@@ -0,0 +1,20 @@
+//
+//  GraphApi.swift
+//  TTSwiftHub
+//
+//  Created by TT on 2021/2/24.
+//  Copyright © 2021 tTao. All rights reserved.
+//
+
+import Foundation
+
+class TTGraphApi: TTSwiftHubAPI {
+    
+    let restApi: TTRestApi
+    let token: String
+    
+//    lazy var networkTransport:  = {
+//        <#statements#>
+//        return <#value#>
+//    }()
+}
