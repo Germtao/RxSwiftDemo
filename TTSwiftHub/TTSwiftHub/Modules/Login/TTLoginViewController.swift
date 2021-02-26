c5fe30ed2333ed805922a84315834844069d4480
QDSG <piftao1127@163.com>
2021-02-26 15:00:16 +0800

    Login Page

--- TTSwiftHub/TTSwiftHub/Modules/Login/TTLoginViewController.swift
+++ TTSwiftHub/TTSwiftHub/Modules/Login/TTLoginViewController.swift
@@ -22,35 +22,49 @@ class TTLoginViewController: TTBaseViewController {
     override func makeUI() {
         super.makeUI()
         
+        navigationItem.titleView = segmentedControl
+        
         languageChanged
             .subscribe(onNext: { [weak self] () in
+                self?.segmentedControl.sectionTitles = [
+                    TTLoginSegments.oAuth.title,
+                    TTLoginSegments.personal.title,
+                    TTLoginSegments.basic.title
+                ]
+                
+                // MARK: - Basic
                 self?.loginTextField.placeholder = R.string.localizable.loginLoginTextFieldPlaceholder.key.localized()
                 self?.passwordTextField.placeholder = R.string.localizable.loginPasswordTextFieldPlaceholder.key.localized()
                 self?.basicLoginButton.titleForNormal = R.string.localizable.loginBasicLoginButtonTitle.key.localized()
+                
+                // MARK: - Personal
+                self?.personalTitleLabel.text = R.string.localizable.loginTitleLabelText.key.localized()
+                self?.personalDetailLabel.text = R.string.localizable.loginDetailLabelText.key.localized()
+                self?.personalTokenTextField.placeholder = R.string.localizable.loginPersonalTokenTextFieldPlaceholder.key.localized()
+                self?.personalLoginButton.titleForNormal = R.string.localizable.loginPersonalLoginButtonTitle.key.localized()
+                
+                // MARK: - OAuth
                 self?.titleLabel.text = R.string.localizable.loginTitleLabelText.key.localized()
                 self?.detailLabel.text = R.string.localizable.loginDetailLabelText.key.localized()
                 self?.oAuthLoginButton.titleForNormal = R.string.localizable.loginOAuthloginButtonTitle.key.localized()
-                self?.segmentedControl.sectionTitles = [TTLoginSegments.oAuth.title, TTLoginSegments.basic.title]
-                self?.navigationItem.titleView = self?.segmentedControl
             })
             .disposed(by: rx.disposeBag)
         
         stackView.removeFromSuperview()
         scrollView.addSubview(stackView)
         stackView.snp.makeConstraints { (make) in
-            make.top.bottom.equalToSuperview().inset(self.inset * 2)
+            make.edges.equalToSuperview().inset(inset * 2)
             make.centerX.equalToSuperview()
-            make.width.equalTo(300)
         }
         
         themeService.rx
-            .bind({ $0.text }, to: titleLabel.rx.textColor)
-            .bind({ $0.textGray }, to: detailLabel.rx.textColor)
-            .bind({ $0.text }, to: basicLogoImageView.rx.tintColor)
-            .bind({ $0.text }, to: oAuthLogoImageView.rx.tintColor)
+            .bind({ $0.text }, to: [titleLabel.rx.textColor, personalTitleLabel.rx.textColor])
+            .bind({ $0.textGray }, to: [detailLabel.rx.textColor, personalDetailLabel.rx.textColor])
+            .bind({ $0.text }, to: [basicLogoImageView.rx.tintColor, personalLogoImageView.rx.tintColor, oAuthLogoImageView.rx.tintColor])
             .disposed(by: rx.disposeBag)
         
         stackView.addArrangedSubview(basicLoginStackView)
+        stackView.addArrangedSubview(personalLoginStackView)
         stackView.addArrangedSubview(oAuthLoginStackView)
         
         bannerView.isHidden = true
@@ -65,6 +79,7 @@ class TTLoginViewController: TTBaseViewController {
         let input = TTLoginViewModel.Input(
             segmentSelection: segmentSelected.asDriverOnErrorJustComplete(),
             basicLoginTrigger: basicLoginButton.rx.tap.asDriver(),
+            personalLoginTrigger: personalLoginButton.rx.tap.asDriver(),
             oAuthLoginTrigger: oAuthLoginButton.rx.tap.asDriver()
         )
         let output = viewModel.transform(input: input)
@@ -81,11 +96,17 @@ class TTLoginViewController: TTBaseViewController {
         output.basicLoginButtonEnabled
             .drive(basicLoginButton.rx.isEnabled)
             .disposed(by: rx.disposeBag)
+        output.personalLoginButtonEnabled
+            .drive(personalLoginButton.rx.isEnabled)
+            .disposed(by: rx.disposeBag)
         
+        // MARK: - <-> 双向绑定
         _ = loginTextField.rx.textInput <-> viewModel.login
         _ = passwordTextField.rx.textInput <-> viewModel.password
+        _ = personalTokenTextField.rx.textInput <-> viewModel.personalToken
         
         output.hidesBasicLoginView.drive(basicLoginStackView.rx.isHidden).disposed(by: rx.disposeBag)
+        output.hidesPersonalLoginView.drive(personalLoginStackView.rx.isHidden).disposed(by: rx.disposeBag)
         output.hidesOAuthLoginView.drive(oAuthLoginStackView.rx.isHidden).disposed(by: rx.disposeBag)
         
         error
@@ -104,24 +125,21 @@ class TTLoginViewController: TTBaseViewController {
     }
     
     lazy var segmentedControl: TTSegmentedControl = {
-        let items = [TTLoginSegments.oAuth.title, TTLoginSegments.basic.title]
+        let items = [TTLoginSegments.oAuth.title, TTLoginSegments.personal.title, TTLoginSegments.basic.title]
         let view = TTSegmentedControl(sectionTitles: items)
         view.selectedSegmentIndex = 0
         view.snp.makeConstraints({ make in
-            make.width.equalTo(250)
+            make.width.equalTo(300)
         })
         return view
     }()
     
+    // MARK: - Basic authentication
+    
     lazy var basicLoginStackView: TTStackView = {
         let subviews: [UIView] = [basicLogoImageView, loginTextField, passwordTextField, basicLoginButton]
         let view = TTStackView(arrangedSubviews: subviews)
-        return view
-    }()
-    
-    lazy var oAuthLoginStackView: TTStackView = {
-        let subviews: [UIView] = [oAuthLogoImageView, titleLabel, detailLabel, oAuthLoginButton]
-        let view = TTStackView(arrangedSubviews: subviews)
+        view.spacing = inset * 2
         return view
     }()
     
@@ -149,11 +167,21 @@ class TTLoginViewController: TTBaseViewController {
         return button
     }()
     
+    // MARK: - OAuth authentication
+    
+    lazy var oAuthLoginStackView: TTStackView = {
+        let subviews: [UIView] = [oAuthLogoImageView, titleLabel, detailLabel, oAuthLoginButton]
+        let view = TTStackView(arrangedSubviews: subviews)
+        view.spacing = inset * 2
+        return view
+    }()
+    
     lazy var oAuthLogoImageView = TTImageView(image: R.image.image_no_result()?.template)
     
     lazy var titleLabel: TTLabel = {
         let label = TTLabel()
         label.font = label.font.withSize(22)
+        label.numberOfLines = 0
         label.textAlignment = .center
         return label
     }()
@@ -167,6 +195,46 @@ class TTLoginViewController: TTBaseViewController {
     }()
     
     lazy var oAuthLoginButton: TTButton = {
+        let button = TTButton()
+        button.imageForNormal = R.image.icon_button_github()
+        button.centerTextAndImage(spacing: inset)
+        return button
+    }()
+    
+    // MARK: - Personal Access Token authentication
+    
+    lazy var personalLoginStackView: TTStackView = {
+//        let subviews: [UIView]
+        return <#value#>
+    }()
+    
+    lazy var personalLogoImageView = TTImageView(image: R.image.image_no_result()?.template)
+    
+    lazy var personalTitleLabel: TTLabel = {
+        let label = TTLabel()
+        label.font = label.font.withSize(22)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy var personalDetailLabel: TTLabel = {
+        let label = TTLabel()
+        label.font = label.font.withSize(17)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy var personalTokenTextField: TTTextField = {
+        let textField = TTTextField()
+        textField.textAlignment = .center
+        textField.keyboardType = .emailAddress
+        textField.autocapitalizationType = .none
+        return textField
+    }()
+    
+    lazy var personalLoginButton: TTButton = {
         let button = TTButton()
         button.imageForNormal = R.image.icon_button_github()
         button.centerTextAndImage(spacing: inset)
--- TTSwiftHub/TTSwiftHub/Modules/Login/TTLoginViewModel.swift
+++ TTSwiftHub/TTSwiftHub/Modules/Login/TTLoginViewModel.swift
@@ -15,11 +15,12 @@ private let loginURL = URL(string: "http://github.com/login/oauth/authorize?clie
 private let callbackURLScheme = "swifthub"
 
 enum TTLoginSegments: Int {
-    case oAuth, basic
+    case oAuth, personal, basic
     
     var title: String {
         switch self {
         case .oAuth: return R.string.localizable.loginOAuthSegmentTitle.key.localized()
+        case .personal: return R.string.localizable.loginPersonalSegmentTitle.key.localized()
         case .basic: return R.string.localizable.loginBasicSegmentTitle.key.localized()
         }
     }
@@ -29,19 +30,22 @@ class TTLoginViewModel: TTViewModel, TTViewModelType {
     struct Input {
         let segmentSelection: Driver<TTLoginSegments>
         let basicLoginTrigger: Driver<Void>
+        let personalLoginTrigger: Driver<Void>
         let oAuthLoginTrigger: Driver<Void>
     }
     
     struct Output {
-        let basicLoginTrigger: Driver<Void>
-        let oAuthLoginTrigger: Driver<Void>
         let basicLoginButtonEnabled: Driver<Bool>
+        let personalLoginButtonEnabled: Driver<Bool>
         let hidesBasicLoginView: Driver<Bool>
+        let hidesPersonalLoginView: Driver<Bool>
         let hidesOAuthLoginView: Driver<Bool>
     }
     
     let login = BehaviorRelay(value: "")
     let password = BehaviorRelay(value: "")
+    
+    let personalToken = BehaviorRelay(value: "")
     
     let code = PublishSubject<String>()
     let tokenSaved = PublishSubject<Void>()
@@ -68,6 +72,15 @@ class TTLoginViewModel: TTViewModel, TTViewModelType {
             }
         }).disposed(by: rx.disposeBag)
         
+        input.personalLoginTrigger
+            .drive(onNext: { [weak self] () in
+                if let personalToken = self?.personalToken.value {
+                    TTAuthManager.setToken(token: TTToken(personalToken: personalToken))
+                    self?.tokenSaved.onNext(())
+                }
+            })
+            .disposed(by: rx.disposeBag)
+        
         let oAuthLoginTrigger = input.oAuthLoginTrigger
         oAuthLoginTrigger.drive(onNext: { [weak self] () in
             self?.authSession = SFAuthenticationSession(url: loginURL, callbackURLScheme: callbackURLScheme, completionHandler: { (callbackUrl, error) in
@@ -84,56 +97,58 @@ class TTLoginViewModel: TTViewModel, TTViewModelType {
             
         }).disposed(by: rx.disposeBag)
         
-        code.flatMapLatest { code -> Observable<TTToken> in
+        let tokenRequest = code.flatMapLatest { code -> Observable<RxSwift.Event<TTToken>> in
             let clientId = Keys.github.appId
             let clientSecret = Keys.github.apiKey
             return self.provider.createAccessToken(clientId: clientId, clientSecret: clientSecret, code: code, redirectUri: nil, state: nil)
                 .trackActivity(self.loading)
                 .trackError(self.error)
-        }.subscribe { [weak self] event in
-            switch event {
-            case .next(let token):
-                TTAuthManager.setToken(token: token)
-                self?.tokenSaved.onNext(())
-            case .error(let error):
-                logError(error.localizedDescription)
-            default: break
-            }
-        }.disposed(by: rx.disposeBag)
+                .materialize()
+        }.share()
+        tokenRequest.elements().subscribe(onNext: { [weak self] token in
+            TTAuthManager.setToken(token: token)
+            self?.tokenSaved.onNext(())
+        }).disposed(by: rx.disposeBag)
+        tokenRequest.errors().bind(to: serverError).disposed(by: rx.disposeBag)
         
-        tokenSaved.flatMapLatest { () -> Observable<RxSwift.Event<TTUser>> in
+        let profileRequest = tokenSaved.flatMapLatest { () -> Observable<RxSwift.Event<TTUser>> in
             return self.provider.profile()
                 .trackActivity(self.loading)
-                .trackError(self.error)
                 .materialize()
-        }.subscribe(onNext: { event in
-            switch event {
-            case .next(let user):
-                user.save()
-                TTAuthManager.tokenValidated()
-                if let login = user.login, let type = TTAuthManager.shared.token?.type().description {
-                    analytics.log(.login(login: login, type: type))
-                }
-                TTApplication.shared.presentInitialScreen(in: TTApplication.shared.window)
-            case .error(let error):
-                logError(error.localizedDescription)
-                TTAuthManager.removeToken()
-            default: break
+        }.share()
+        profileRequest.elements().subscribe(onNext: { user in
+            user.save()
+            TTAuthManager.tokenValidated()
+            if let login = user.login, let type = TTAuthManager.shared.token?.type().description {
+                analytics.log(.login(login: login, type: type))
             }
+            TTApplication.shared.presentInitialScreen(in: TTApplication.shared.window)
         }).disposed(by: rx.disposeBag)
+        profileRequest.errors().bind(to: serverError).disposed(by: rx.disposeBag)
+        
+        serverError
+            .subscribe(onNext: { (error) in
+                TTAuthManager.removeToken()
+            })
+            .disposed(by: rx.disposeBag)
         
         let basicLoginButtonEnabled = BehaviorRelay.combineLatest(login, password, loading.asObservable()) {
             return $0.isNotEmpty && $1.isNotEmpty && !$2
         }.asDriver(onErrorJustReturn: false)
         
+        let personalLoginButtonEnabled = BehaviorRelay.combineLatest(personalToken, loading.asObservable()) {
+            return $0.isNotEmpty && !$1
+        }.asDriver(onErrorJustReturn: false)
+        
         let hidesBasicLoginView = input.segmentSelection.map { $0 != TTLoginSegments.basic }
+        let hidesPersonalLoginView = input.segmentSelection.map { $0 != TTLoginSegments.personal }
         let hidesOAuthLoginView = input.segmentSelection.map { $0 != TTLoginSegments.oAuth }
         
         return Output(
-            basicLoginTrigger: basicLoginTrigger,
-            oAuthLoginTrigger: oAuthLoginTrigger,
             basicLoginButtonEnabled: basicLoginButtonEnabled,
+            personalLoginButtonEnabled: personalLoginButtonEnabled,
             hidesBasicLoginView: hidesBasicLoginView,
+            hidesPersonalLoginView: hidesPersonalLoginView,
             hidesOAuthLoginView: hidesOAuthLoginView
         )
     }
